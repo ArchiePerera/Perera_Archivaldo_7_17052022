888d810497d22e3ee9aa791ec67e43798241827a
ArchiePerera <archivaldoperera@gmail.com>
2022-05-20 18:00:24 +0200

    Security implémentation - bcrypt, jsonwebtoken, helmet, express-rate-limit - Routes User

--- back/app.js
+++ back/app.js
@@ -1,15 +1,41 @@
+// Mise à disposition des modules de fonctionnement de l'API
+
 const express = require('express');
+const path = require("path");
+
+// Initialisation du module Express
 
 const app = express();
+app.use(express.json());
+
+//--------------------- SÉCURITÉ ----------------------
+
+// Implémentation de CORS - All Origin - Header's Default
 
 const cors = require('cors');
-
-app.use(express.json());
 app.use(cors());
+
+// Initialisation d'Helmet (Sécurisation des headers)
+
+const helmet = require('helmet');
+app.use(helmet({ 
+    crossOriginResourcePolicy: { policy: "same-site" } 
+}));
+
+// Initialisation du limiteur de requêtes à 100 sur 1h
+
+const rateLimit = require("express-rate-limit");
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000, //1h
+    message: "Too many request from this IP"
+});
+
+//----------------- BASE DE DONNÉES -------------------
 
 const db = require('./models');
 
-const users = require('./routes/users');
+// Connexion à la BDD
 
 db.sequelize
     .sync()
@@ -19,9 +45,25 @@ db.sequelize
         });
     })
     .catch((err) => {
-        console.log('Error connecting :${err.message}');
+        console.log(`Error connecting :${err.message}`);
     });
 
-app.use('/users', users)
+//----------------- ROUTES -----------------------------
+
+// Mise à disposition du chemin vers le répertoire profil utilisateur
+
+app.use("/images/profiles", express.static(path.join(__dirname, "profiles")));
+
+// Mise à disposition du chemin vers le répertoire feeds
+
+app.use("/images/feeds", express.static(path.join(__dirname, "feeds")));
+
+// Mise à disposition des fichiers routes
+
+const userRoutes = require('./routes/userRoutes');
+
+// Routes
+
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
--- back/routes/userRoutes.js
+++ back/routes/userRoutes.js
@@ -1,14 +1,16 @@
+
+
 const express = require('express');
 const router = express.Router();
 
 const { Users } = require("../models");
 
-router.get('/', async (req, res) => {
+router.get('/users', async (req, res) => {
     const users = await Users.findAll();
     res.json(users);
 });
 
-router.post('/', async (req, res) => {
+router.post('/signin', async (req, res) => {
     const values = req.body;
     const createdUser = await Users.create(values);
     res.json(createdUser);
